96b326565162ebd1d4ef80cf910b3601e2a5b29d
ChrisMagnuson <hourglasssand@gmail.com>
2017-06-28 13:18:00 -0400

    Refactor to singular noun, add Remove-TervisSQLAnywhereConnection, Get-TervisSQLAnywhereConnectionString

--- TervisSQLAnywherePowerShell.psm1
+++ TervisSQLAnywherePowerShell.psm1
@@ -1,11 +1,29 @@
-﻿function Get-TervisSQLAnywhereConnections {
+﻿function Get-TervisSQLAnywhereConnection {
     param (
         [Parameter(Mandatory)]$EnvironmentName
     )
+    $ConnectionString = Get-TervisSQLAnywhereConnectionString -EnvironmentName $EnvironmentName
+    Get-SQLAnywhereConnection -ConnectionString $ConnectionString
+}
 
+function Remove-TervisSQLAnywhereConnection {
+    param (
+        [Parameter(Mandatory)]$EnvironmentName,
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]$ID
+    )
+    begin {
+        $ConnectionString = Get-TervisSQLAnywhereConnectionString -EnvironmentName $EnvironmentName
+    }
+    process {        
+        Remove-SQLAnywhereConnection -ConnectionString $ConnectionString -ID $ID
+    }
+}
+
+function Get-TervisSQLAnywhereConnectionString {
+    param (
+        [Parameter(Mandatory)]$EnvironmentName
+    )
     $WCSEnvironmentState = Get-WCSEnvironmentState -EnvironmentName $EnvironmentName
     $SybaseDatabaseEntryDetails = Get-PasswordstateSybaseDatabaseEntryDetails -PasswordID $WCSEnvironmentState.SybaseQCUserPasswordEntryID
-    $ConnectionString = $SybaseDatabaseEntryDetails | ConvertTo-SQLAnywhereConnectionString
-
-    Get-SQLAnywhereConnections -ConnectionString $ConnectionString
+    $SybaseDatabaseEntryDetails | ConvertTo-SQLAnywhereConnectionString
 }
