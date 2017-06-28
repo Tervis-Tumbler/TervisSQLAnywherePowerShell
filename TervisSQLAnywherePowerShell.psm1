e12e1ee6e64dca498445a4fd71555e0120fb0b24
ChrisMagnuson <hourglasssand@gmail.com>
2017-06-28 10:23:59 -0400

    Add Get-TervisSQLAnywhereConnections

---
+++ TervisSQLAnywherePowerShell.psm1
@@ -0,0 +1,11 @@
+﻿function Get-TervisSQLAnywhereConnections {
+    param (
+        [Parameter(Mandatory)]$EnvironmentName
+    )
+
+    $WCSEnvironmentState = Get-WCSEnvironmentState -EnvironmentName $EnvironmentName
+    $SybaseDatabaseEntryDetails = Get-PasswordstateSybaseDatabaseEntryDetails -PasswordID $WCSEnvironmentState.SybaseQCUserPasswordEntryID
+    $ConnectionString = $SybaseDatabaseEntryDetails | ConvertTo-SQLAnywhereConnectionString
+
+    Get-SQLAnywhereConnections -ConnectionString $ConnectionString
+}
