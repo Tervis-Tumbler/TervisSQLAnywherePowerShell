1e4771660b9cea3c4ea4067b4b353423c2d4cfe9
Hamilton Perez <hambone124@users.noreply.github.com>
2018-05-02 13:44:22 -0400

    Added Invoke-TervisDBUnloadToWCSGitRepository

--- TervisSQLAnywherePowerShell.psm1
+++ TervisSQLAnywherePowerShell.psm1
@@ -23,7 +23,36 @@ function Get-TervisSQLAnywhereConnectionString {
     param (
         [Parameter(Mandatory)]$EnvironmentName
     )
-    $WCSEnvironmentState = Get-WCSEnvironmentState -EnvironmentName $EnvironmentName
-    $SybaseDatabaseEntryDetails = Get-PasswordstateSybaseDatabaseEntryDetails -PasswordID $WCSEnvironmentState.SybaseQCUserPasswordEntryID
+    $SybaseDatabaseEntryDetails = Get-TervisSQLAnywhereCredential -EnvironmentName $EnvironmentName
     $SybaseDatabaseEntryDetails | ConvertTo-SQLAnywhereConnectionString
+}
+
+function Get-TervisSQLAnywhereCredential {
+    param (
+        [Parameter(Mandatory)]$EnvironmentName
+        )
+    $WCSEnvironmentState = Get-WCSEnvironmentState -EnvironmentName $EnvironmentName
+    Get-PasswordstateSybaseDatabaseEntryDetails -PasswordID $WCSEnvironmentState.SybaseQCUserPasswordEntryID
+}
+
+function Invoke-DBUnloadToWCSGitRepository {
+    param (
+        [Parameter(Mandatory)]$EnvironmentName
+    )
+
+    $ConnectionString = Get-TervisSQLAnywhereConnectionString -EnvironmentName $EnvironmentName
+    $SybaseDatabaseEntryDetails = Get-TervisSQLAnywhereCredential -EnvironmentName $EnvironmentName
+    $DBUnloadOutputPath = "C:\dbunload.sql"
+    $DBUnloadOutputPathRemote = $DBUnloadOutputPath | ConvertTo-RemotePath -ComputerName $SybaseDatabaseEntryDetails.ServerName
+    $DBUnloadDestinationPath = "$(Get-WCSJavaApplicationGitRepositoryPath)\DBUnload\$($EnvironmentName)"
+    
+    Invoke-Command -ComputerName $SybaseDatabaseEntryDetails.ServerName -ScriptBlock {
+        dbunload -no -c "$using:ConnectionString" -r "$using:DBUnloadOutputPath" -y
+    }
+
+    try {
+        Move-Item -Path $DBUnloadOutputPathRemote -Destination $DBUnloadDestinationPath -Force -ErrorAction Stop
+    } catch {
+        throw "No SQL file was generated from DBUnload"
+    }
 }
